d4cf50f5f7cf4f13a5cccaddcfaf8dba2f45a5a5
DESKTOP-ACFEFB8\USER <dahunj@gmail.com>
2025-06-12 18:52:35 +0900

    4813 알람 없애기

--- CMI8000/SequenceMain.cpp
+++ CMI8000/SequenceMain.cpp
@@ -10132,7 +10132,7 @@ BOOL CSequenceMain::EmptyTrayElevator_Run()
 			if (g_objCommon.Check_Position(AX_EMPTY_PORT_Z, 0)) m_nEmptyTrayElCase = 20;
 			else if (g_objCommon.Check_Position(AX_EMPTY_PORT_Z, 1)) m_nEmptyTrayElCase = 21;
 			else if (g_objCommon.Check_Position(AX_EMPTY_PORT_Z, 2, 1.5) && !m_pDX01->iEmptyPortTopCheck && !m_pDX01->iEmptyPortExist) m_nEmptyTrayElCase++;
-			else if (m_pDX01->iEmptyPortTopCheck)
+			else 
 			{ 
 				g_objCommon.Move_Position(AX_EMPTY_PORT_Z, 0);
 				m_nEmptyTrayElCase = 20;
