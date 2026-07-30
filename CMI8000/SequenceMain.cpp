cbed957e15f3d45de8cc62b71dce539a5179913e
dahunj <dahunj@nate.com>
2026-07-30 14:46:54 +0900

    수정완

--- CMI8000/SequenceMain.cpp
+++ CMI8000/SequenceMain.cpp
@@ -3857,8 +3857,8 @@ BOOL CSequenceMain::Btm1Picker_Run()
 
 	case 1:		// Picker 
 		if(!g_objAJinAXL.Is_Done(AX_BTM1_PICKER_Z)) break;
-		if(!g_objCommon.Get_InfoBtm1PickerClose()) break;
-		//if(m_bBtm1ModulePick && !g_objCommon.Get_InfoBtm1PickerClose())break;
+		if(!m_tBtm1PickLoop.Waiting_Time(100)) break;
+		if(!g_objCommon.Get_InfoBtm1PickerClose()) break; 		
 		if ((g_objCommon.Check_Position(AX_BTM1_PICKER_X, 0) || g_objCommon.Check_Position(AX_BTM1_PICKER_X, 1) ||
 			g_objCommon.Check_Position(AX_BTM1_PICKER_X, 2) || g_objCommon.Check_Position(AX_BTM1_PICKER_X, 3)) && 
 			g_objCommon.Check_Position(AX_BTM1_PICKER_P1, 0) && g_objCommon.Check_Position(AX_BTM1_PICKER_P2, 0))
@@ -4066,6 +4066,7 @@ BOOL CSequenceMain::Btm1Picker_Run()
 		if ( g_objCommon.Check_Position(AX_BTM1_PICKER_Z, 0) && g_objCommon.Get_InfoBtm1PickerVacOn() 
 			&& g_objCommon.Get_InfoBtm1PickerClose())
 		{
+			if(!m_tBtm1PickLoop.Waiting_Time(100)) break;
 			int nBtmX = 4 * nB1pRow;
 
 			g_objLogFile.Save_PositionLog(gData.nPNoBtm1Pick, gData.nTNoBtm1Pick[nBtmX], -1
