731d836ad58c257a2fa330466e2d4bb35139477d
dahunj <dahunj@nate.com>
2026-08-11 10:38:41 +0900

    MES 보고 오류 수정

--- CMI8000/SequenceMain.cpp
+++ CMI8000/SequenceMain.cpp
@@ -2173,6 +2173,9 @@ void CSequenceMain::Job_LotEnd(int nPortNo, int nGTNo)
 	gData.nTrayUseCount[nPx] = 0;
 	gData.nCmUseCount[nPx] = 0;
 
+	for (int i = 0; i < 30; i++) for (int j = 0; j < 40; j++) gMes.sBarID[nPx][i][j] = gMes.sJudge[nPx][i][j] = gMes.sNGCode[nPx][i][j] = "";
+	for (int i = 0; i < 30; i++) for (int j = 0; j < 40; j++) for (int k = 0; k < 7; k++) gData.sNGData[nPx][i][j][k] = "";
+
 	gData.nTrayCntNG[nPx][2] = 1;
 	gData.nTrayCntNG[nPx][3] = 1;
 	gData.nTrayCntNG[nPx][0] = gData.nTrayCntNG[nPx][2] + 1;
