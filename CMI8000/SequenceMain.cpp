c2eab4488795f188b6584360d722a419ab7a6d16
dahunj <dahunj@nate.com>
2026-08-13 13:57:50 +0900

    TimeOut 시 GMES 보고 수정

--- CMI8000/SequenceMain.cpp
+++ CMI8000/SequenceMain.cpp
@@ -1439,6 +1439,7 @@ BOOL CSequenceMain::Check_InspectDone(int nPortNo, int nTrayNo, int nCmNo, int &
 				// SortPicker에서 검사 완료 체크할때 검사결과가 안날라왔으면 1차로 빼준다.
 				gData.nInspectInfo[nPx][nTx][nCx] = 4;
 				gMes.sJudge[nPx][nTx][nCx] = "N1";
+				gMes.sNGCode[nPx][nTx][nCx].Empty();
 				strLog.Format("Judge Time Over Sort Picker, PortNo(%d), TrayNo(%d), CmNo(%d)", nPx+1, nTx+1, nCx+1);
 				g_objLogFile.Save_HandlerLog(strLog);
 			} 
@@ -1576,6 +1577,7 @@ BOOL CSequenceMain::Check_InspectDone2(int nPortNo, int nTrayNo, int nCmNo, int 
 			if (m_pEquipData->bUseInspectSkip || ((dwTick - gData.dwSkipTime_Sort2)  > m_pEquipData->nDelayAdd[4]) ) {	// SortPicker에서 검사 완료 체크할때 검사결과가 안날라왔으면 1차로 빼준다.
 				gData.nInspectInfo[nPx][nTx][nCx] = 4;
 				gMes.sJudge[nPx][nTx][nCx] = "N1";
+				gMes.sNGCode[nPx][nTx][nCx].Empty();
 				strLog.Format("Judge Time Over Sort Picker, PortNo(%d), TrayNo(%d), CmNo(%d)", nPx+1, nTx+1, nCx+1);
 				g_objLogFile.Save_HandlerLog(strLog);
 			} else {
@@ -2172,6 +2174,9 @@ void CSequenceMain::Job_LotEnd(int nPortNo, int nGTNo)
 	gData.nTrayCntNG[nPx][3] = 1;
 	gData.nTrayCntNG[nPx][0] = gData.nTrayCntNG[nPx][2] + 1;
 	gData.nTrayCntNG[nPx][1] = gData.nTrayCntNG[nPx][3] + 1;
+
+	for (int i = 0; i < 30; i++) for (int j = 0; j < 40; j++) gMes.sBarID[nPx][i][j] = gMes.sJudge[nPx][i][j] = gMes.sNGCode[nPx][i][j] = "";
+	for (int i = 0; i < 30; i++) for (int j = 0; j < 40; j++) for (int k = 0; k < 7; k++) gData.sNGData[nPx][i][j][k] = "";
 
 
 	
