f00f9c67ffd7d8618bdddb709928f285df6e667a
dahunj <dahunj@nate.com>
2026-08-27 11:59:35 +0900

    fix

--- CMI8000/SequenceMain.cpp
+++ CMI8000/SequenceMain.cpp
@@ -4664,28 +4664,30 @@ BOOL CSequenceMain::Inspection1_Run()
 	case -1: //Slave Close
 		if(!m_pDX06->iCmAlignMasterOpen && m_pDX06->iCmAlignMasterClose)
 		{
-			m_pDY06->oCmAlignSlaveOpen = FALSE;  m_pDY06->oCmAlignSlaveClose = TRUE;
-			g_objAJinAXL.Write_Output(6);
-
 			m_tInspect1Loop.Takt_End(nTaktZone,3); 
 			m_tInspect1Loop.Takt_Start(nTaktZone,9); 
+			g_objCommon.Set_InspectVacOff(1, 0);	// Vac Off
 			m_nInspect1Case = -2; m_tInspect1Loop.Set_LoopTime(10000);
 			bTaktSave1 = FALSE;
 		}
 		break;
 	case -2:
 		if (g_objCommon.Check_Position(AX_INSPECT_STAGE1_X, 1) && g_objCommon.Check_Position(AX_MODULE_ALIGN_Z, 1) &&
-			!m_pDX06->iCmAlignMasterOpen && m_pDX06->iCmAlignMasterClose &&
-			!m_pDX06->iCmAlignSlaveOpen  && m_pDX06->iCmAlignSlaveClose) 
+			!m_pDX06->iCmAlignMasterOpen && m_pDX06->iCmAlignMasterClose ) 
 		{		
-			g_objCommon.Set_InspectVacOff(1, 0);	// Vac Off
+			if(!m_tInspect1Loop.Waiting_Time(m_pEquipData->nVacOffDelay[4])) break;
+			m_pDY06->oCmAlignSlaveOpen = FALSE;  m_pDY06->oCmAlignSlaveClose = TRUE;
+			g_objAJinAXL.Write_Output(6);
 			m_nInspect1Case = -3; m_tInspect1Loop.Set_LoopTime(10000);
 		}
 		break;
 	case -3:
-		if(!m_tInspect1Loop.Waiting_Time(m_pEquipData->nVacOffDelay[4])) break;
-		g_objCommon.Set_InfoInspectVacOn(1, 1);	// Vac On
-		m_nInspect1Case = 4; m_tInspect1Loop.Set_LoopTime(10000);
+		//if(!m_tInspect1Loop.Waiting_Time(m_pEquipData->nVacOffDelay[4])) break;
+		if(!m_pDX06->iCmAlignSlaveOpen  && m_pDX06->iCmAlignSlaveClose) 
+		{
+			g_objCommon.Set_InfoInspectVacOn(1, 1);	// Vac On
+			m_nInspect1Case = 4; m_tInspect1Loop.Set_LoopTime(10000);
+		}		
 		break;
 
 	case 4:		// CM Align Vac On and Slave Open
@@ -4693,6 +4695,7 @@ BOOL CSequenceMain::Inspection1_Run()
 			!m_pDX06->iCmAlignMasterOpen && m_pDX06->iCmAlignMasterClose &&
 			!m_pDX06->iCmAlignSlaveOpen  && m_pDX06->iCmAlignSlaveClose ) 
 		{				
+			if (!m_tInspect1Loop.Waiting_Time(500)) return TRUE;
 			m_pDY06->oCmAlignSlaveOpen = TRUE;  m_pDY06->oCmAlignSlaveClose = FALSE;
 			g_objAJinAXL.Write_Output(6);
 
@@ -4704,8 +4707,8 @@ BOOL CSequenceMain::Inspection1_Run()
 		break;
 	case 5:
 		if(m_pDX06->iCmAlignSlaveOpen && !m_pDX06->iCmAlignSlaveClose )
-		{
-			if (!m_tInspect1Loop.Waiting_Time(300)) return TRUE;
+		{			
+			if (!m_tInspect1Loop.Waiting_Time(100)) return TRUE;
 			m_pDY06->oCmAlignMasterOpen = TRUE;  m_pDY06->oCmAlignMasterClose = FALSE;
 			g_objAJinAXL.Write_Output(6);
 
@@ -5264,37 +5267,36 @@ BOOL CSequenceMain::Inspection2_Run()
 		break;
 	case -1: //Slave Close
 		if(!m_pDX06->iCmAlignMasterOpen && m_pDX06->iCmAlignMasterClose)
-		{		
-			//g_objCommon.Set_InspectVacOff(2, 0);	// Vac Off
-			//if(!m_tInspect2Loop.Waiting_Time(400)) break;
-
-			m_pDY06->oCmAlignSlaveOpen = FALSE;  m_pDY06->oCmAlignSlaveClose = TRUE;
-			g_objAJinAXL.Write_Output(6);
-			m_nInspect2Case = -2; m_tInspect2Loop.Set_LoopTime(10000);
-			
+		{			
 			m_tInspect2Loop.Takt_End(nTaktZone,3); 
 			m_tInspect2Loop.Takt_Start(nTaktZone,9); 
+			g_objCommon.Set_InspectVacOff(2, 0);	// Vac Off
+			m_nInspect2Case = -2; m_tInspect2Loop.Set_LoopTime(10000);		
 		}
 		break;
 	case -2:
 		if (g_objCommon.Check_Position(AX_INSPECT_STAGE2_X, 1) && g_objCommon.Check_Position(AX_MODULE_ALIGN_Z, 1) &&
-			!m_pDX06->iCmAlignMasterOpen && m_pDX06->iCmAlignMasterClose &&
-			!m_pDX06->iCmAlignSlaveOpen  && m_pDX06->iCmAlignSlaveClose) 
+			!m_pDX06->iCmAlignMasterOpen && m_pDX06->iCmAlignMasterClose ) 
 		{		
-			g_objCommon.Set_InspectVacOff(2, 0);	// Vac Off
+			if(!m_tInspect2Loop.Waiting_Time(m_pEquipData->nVacOffDelay[4])) break;
+			m_pDY06->oCmAlignSlaveOpen = FALSE;  m_pDY06->oCmAlignSlaveClose = TRUE;
+			g_objAJinAXL.Write_Output(6);
 			m_nInspect2Case = -3; m_tInspect2Loop.Set_LoopTime(10000);
 		}
 		break;
 	case -3:
-		if(!m_tInspect2Loop.Waiting_Time(m_pEquipData->nVacOffDelay[4])) break;
-		g_objCommon.Set_InfoInspectVacOn(2, 1);	// Vac On
-		m_nInspect2Case = 4; m_tInspect2Loop.Set_LoopTime(10000);
+		if(!m_pDX06->iCmAlignSlaveOpen  && m_pDX06->iCmAlignSlaveClose)
+		{
+			g_objCommon.Set_InfoInspectVacOn(2, 1);	// Vac On
+			m_nInspect2Case = 4; m_tInspect2Loop.Set_LoopTime(10000);
+		}		
 		break;
 	case 4:		// CM Align Slave Open
 		if (g_objCommon.Check_Position(AX_INSPECT_STAGE2_X, 1) && g_objCommon.Check_Position(AX_MODULE_ALIGN_Z, 1) &&
 			!m_pDX06->iCmAlignMasterOpen && m_pDX06->iCmAlignMasterClose &&
 			!m_pDX06->iCmAlignSlaveOpen  && m_pDX06->iCmAlignSlaveClose )
 		{
+			if (!m_tInspect1Loop.Waiting_Time(500)) return TRUE;
 			m_pDY06->oCmAlignSlaveOpen = TRUE;  m_pDY06->oCmAlignSlaveClose = FALSE;			
 			g_objAJinAXL.Write_Output(6);
 			m_nInspect2Case = 5; m_tInspect2Loop.Set_LoopTime(5000); 
@@ -5306,7 +5308,7 @@ BOOL CSequenceMain::Inspection2_Run()
 	case 5:
 		if(m_pDX06->iCmAlignSlaveOpen && !m_pDX06->iCmAlignSlaveClose)
 		{
-			if (!m_tInspect2Loop.Waiting_Time(300)) return TRUE;
+			if (!m_tInspect2Loop.Waiting_Time(100)) return TRUE;
 			m_pDY06->oCmAlignMasterOpen = TRUE;  m_pDY06->oCmAlignMasterClose = FALSE;
 			g_objAJinAXL.Write_Output(6);
 			m_nInspect2Case = 6; m_tInspect2Loop.Set_LoopTime(5000); 
@@ -5854,47 +5856,48 @@ BOOL CSequenceMain::Inspection3_Run()
 	case -1: //Slave Close
 		if(!m_pDX06->iCmAlignMasterOpen && m_pDX06->iCmAlignMasterClose)
 		{		
-			m_pDY06->oCmAlignSlaveOpen = FALSE;  m_pDY06->oCmAlignSlaveClose = TRUE;
-			g_objAJinAXL.Write_Output(6);
-			m_nInspect3Case = -2; m_tInspect3Loop.Set_LoopTime(5000);
-
 			m_tInspect3Loop.Takt_End(nTaktZone,3); 
-			m_tInspect3Loop.Takt_Start(nTaktZone,9); 			
+			m_tInspect3Loop.Takt_Start(nTaktZone,9); 		
+			g_objCommon.Set_InspectVacOff(3, 0);	// Vac Off
+			m_nInspect3Case = -2; m_tInspect3Loop.Set_LoopTime(5000);				
 		}
 		break;
 	case -2:
 		if (g_objCommon.Check_Position(AX_INSPECT_STAGE3_X, 1) && g_objCommon.Check_Position(AX_MODULE_ALIGN_Z, 1) &&
-			!m_pDX06->iCmAlignMasterOpen && m_pDX06->iCmAlignMasterClose &&
-			!m_pDX06->iCmAlignSlaveOpen  && m_pDX06->iCmAlignSlaveClose) 
-		{		
-			g_objCommon.Set_InspectVacOff(3, 0);	// Vac Off
+			!m_pDX06->iCmAlignMasterOpen && m_pDX06->iCmAlignMasterClose ) 
+		{			
+			if(!m_tInspect3Loop.Waiting_Time(m_pEquipData->nVacOffDelay[4])) break;
+			m_pDY06->oCmAlignSlaveOpen = FALSE;  m_pDY06->oCmAlignSlaveClose = TRUE;
+			g_objAJinAXL.Write_Output(6);
 			m_nInspect3Case = -3; m_tInspect3Loop.Set_LoopTime(10000);
 		}
 		break;
 	case -3:
-		if(!m_tInspect3Loop.Waiting_Time(m_pEquipData->nVacOffDelay[4])) break;
-		g_objCommon.Set_InfoInspectVacOn(3, 1);	// Vac On
-		m_nInspect3Case = 4; m_tInspect3Loop.Set_LoopTime(10000);
+		if(!m_pDX06->iCmAlignSlaveOpen  && m_pDX06->iCmAlignSlaveClose)
+		{
+			g_objCommon.Set_InfoInspectVacOn(3, 1);	// Vac On
+			m_nInspect3Case = 4; m_tInspect3Loop.Set_LoopTime(10000);
+		}		
 		break;
 	case 4:		// CM Align Slave Open
 		if (g_objCommon.Check_Position(AX_INSPECT_STAGE3_X, 1) && g_objCommon.Check_Position(AX_MODULE_ALIGN_Z, 1) &&
 			!m_pDX06->iCmAlignMasterOpen && m_pDX06->iCmAlignMasterClose &&
 			!m_pDX06->iCmAlignSlaveOpen  && m_pDX06->iCmAlignSlaveClose  ) 
 		{				
+			if (!m_tInspect3Loop.Waiting_Time(500)) return TRUE;
 			m_pDY06->oCmAlignSlaveOpen = TRUE;  m_pDY06->oCmAlignSlaveClose = FALSE;
 			//m_pDY06->oCmAlignMasterOpen = TRUE;  m_pDY06->oCmAlignMasterClose = FALSE;
 			g_objAJinAXL.Write_Output(6);
 			m_nInspect3Case = 5; m_tInspect3Loop.Set_LoopTime(5000);
 			
 			m_tInspect3Loop.Takt_End(nTaktZone,9); 
-			m_tInspect3Loop.Takt_Start(nTaktZone,4); 
-			
+			m_tInspect3Loop.Takt_Start(nTaktZone,4); 			
 		}
 		break;
 	case 5:
 		if(m_pDX06->iCmAlignSlaveOpen && !m_pDX06->iCmAlignSlaveClose)
 		{
-			if (!m_tInspect3Loop.Waiting_Time(300)) return TRUE;
+			if (!m_tInspect3Loop.Waiting_Time(100)) return TRUE;
 			m_pDY06->oCmAlignMasterOpen = TRUE;  m_pDY06->oCmAlignMasterClose = FALSE;
 			g_objAJinAXL.Write_Output(6);
 			m_nInspect3Case = 6; m_tInspect3Loop.Set_LoopTime(5000); 
